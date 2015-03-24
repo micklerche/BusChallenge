6742d8b58c0ca8fdba9b0630a8a25c3b457e0bbe
GrumpyDane <micklerche@gmail.com>
2015-03-24 13:33:10 -0500

    Initial Commit

---
+++ BSBusChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSBusChallenge
+//
+//  Created by Mick Lerche on 3/24/15.
+//  Copyright (c) 2015 Mick Lerche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
